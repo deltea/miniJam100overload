fb4c373266f2b57d087082b280f778b3818cbd68
thcheetah777 <thcheetah777@gmail.com>
2022-02-18 09:02:01 -0800

    Create viruses

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ class Game extends Phaser.Scene {
   }
   preload() {
     this.load.image("player", "assets/player.png");
+    this.load.image("virus", "assets/virus.png");
   }
   create() {
     // Add my own game engine
@@ -15,6 +16,22 @@ class Game extends Phaser.Scene {
 
     // Create player
     game.player = this.physics.add.sprite(game.engine.gameWidth / 2, game.engine.gameHeight / 2, "player").setGravityY(-1500).setScale(8).setDrag(1500).setCollideWorldBounds(true).setSize(6, 5).setOffset(1, 2);
+
+    // Shoot viruses
+    game.viruses = this.physics.add.group();
+    game.virusInterval = this.time.addEvent({
+      delay: 3000,
+      callback: () => {
+        let virus = game.viruses.create(game.player.x, game.player.y, "virus").setBounce(1).setCollideWorldBounds(true).setScale(8).setGravityY(-1500);
+        this.physics.velocityFromAngle(Math.random() * 360, 200, virus.body.velocity);
+      },
+      callbackScope: this,
+      repeat: -1
+    });
+
+    // ---------- Colliders ----------
+    this.physics.add.collider(game.viruses, game.viruses);
+    this.physics.add.collider(game.player, game.viruses);
   }
   update() {
     // Player movement
